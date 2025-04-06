012122201b8eca6b0bd5a93b2eeb467663d73051
MauroChinellato <mauro.chinellato@ucb.edu.bo>
2025-04-06 19:42:31 -0400

    ejercicio4.cpp

---
+++ ejercicio4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    if(n < 0) {
+        cout << "El numero es negativo. Intentelo de nuevo" << endl;
+    }
+    else if(n > 14) {
+        cout << "El numero es muy grande. Intentelo de nuevo" << endl;
+    }
+    else {
+        int F = 1;
+        for(int i = 1; i <= n; i++) 
+        {
+            F *= i;
+        }
+        cout << factorial << endl;
+    }
+    return 0;
+}
